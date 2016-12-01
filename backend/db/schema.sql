14578321826b70b0e7fc075b14c6781a0eab7138
jbenjaminy <young.benjamin@gmail.com>
2016-12-01 07:54:39 -0600

    updates staff schema and separates staff data into multiple tables

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -45,12 +45,9 @@ create table if not exists hosts (
     email text not null unique 
 );
 
--- managers -- staff with extra access
-    -- can see all events, who has not replied, who's been offered, phone number and contact info
-
-
-create table if not exists staff (
+create table if not exists staff_contact (
     id serial primary key,
+    email text not null unique,
     username text not null unique,
     password text not null,
     first_name text not null,
@@ -60,37 +57,108 @@ create table if not exists staff (
     city text not null,
     state text not null,
     zip integer not null,
-    phone integer not null,
-    email text not null unique,
-    emergency_phone integer not null,
+    phone text not null
+);
+
+create table if not exists staff_application (
+    id serial primary key,
+    staff_id integer not null references staff,
+-- personal details
     dob date not null,
     -- 1999-01-08
-    start_date date not null,
+    emergency_phone text not null,
     us_citizen boolean,
     prior_conviction boolean,
-    ss_copy boolean,
-    dl_copy boolean,
-    dl_num integer,
-    dl_exp date,
-    tabc boolean,
-    tabc_exp date,
-    -- base pay rate tied to tabc
-    -- last 3 positions/relevant experience
-    -- # years catering/food service
-    -- don't need start date
-    -- desired salary
-    -- experience, what they want to be paid, availability, upload resume, ss card
-    w4 boolean,
-    direct_deposit boolean,
-    end_date date, 
-    is_active boolean,
-    is_lead boolean,
-    salary integer,
+    ss_num text not null,
+    dl_num text not null,
+    dl_exp date not null,
+    referred_by text,
+-- position details
     waitstaff boolean,
     bartender boolean,
     valet boolean,
     model boolean,
-    manager boolean,
+    part_time boolean,
+    full_time boolean,
+    seasonal boolean,
+    desired_pay_rate integer,
+    desired_start_date date,
+-- work history / related experience
+    was_prev_empl boolean,
+    prev_position text,
+    prev_pay_rate integer,
+    prev_start_date date,
+    prev_end_date date,
+    num_years_exp integer,
+    tabc_certified boolean,
+    tabc_exp date,
+    past_work_1 text not null,
+    past_title_1 text not null,
+    past_duration_1 integer not null,
+    past_work_2 text,
+    past_title_2 text,
+    past_duration_2 integer,
+    past_work_3 text,
+    past_title_3 text,
+    past_duration_3 integer,
+    ref_name_1 text not null,
+    ref_relation_1 text not null,
+    ref_phone_1 text not null,
+    ref_name_2 text,
+    ref_relation_2 text,
+    ref_phone_2 text,
+    ref_name_3 text,
+    ref_relation_3 text,
+    ref_phone_3 text
+);
+
+create table if not exists staff_documents (
+    id serial primary key,
+    staff_id integer not null references staff,
+-- store images in Amazon S3
+    ss_on_file boolean,
+    ss_url text,
+    dl_on_file boolean,
+    dl_url text,
+    resume_on_file boolean,
+    resume_url text,
+    tabc_on_file boolean,
+    tabc_url text,
+    i9_on_file boolean,
+    i9_url text,
+    w4_on_file boolean,
+    w4_url text,
+    non_compete_on_file boolean,
+    non_compete_url text,
+    conduct_agmt_on_file boolean,
+    conduct_agmt_url text
+);
+
+create table if not exists staff_payroll (
+    id serial primary key,
+    staff_id integer not null references staff,
+    pay_rate integer not null,
+    pay_period_hours integer,
+    overtime_hours integer,
+    all_time_hours integer,
+    direct_deposit boolean,
+    bank text,
+    acct_type text,
+    acct_num text,
+    routing_num text
+);
+
+create table if not exists staff_status (
+    id serial primary key,
+    staff_id integer not null references staff,
+    is_active boolean,
+    is_waitstaff boolean,
+    is_bartender boolean,
+    is_valet boolean,
+    is_model boolean,
+    is_manager boolean,
+    -- managers -- staff with extra access
+        -- can see all events, who has not replied, who's been offered, phone number and contact info
     has_eton boolean,
     has_bistro boolean,
     has_shoes boolean,
@@ -107,6 +175,12 @@ create table if not exists staff (
     has_apron boolean,
     has_bar_tool boolean,
     has_bar_set boolean
+);
+
+create table if not exists staff_availability (
+    id serial primary key,
+    staff_id integer not null references staff,
+    date_unavailable date
 );
 
 create table if not exists events (
